a41b1598fe218d8c95076be05314aa4dbd35b5b4
MaximusMaximos <maximus@opengpu.network>
2025-11-15 06:47:41 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
         </div>
       </section>
 
-   
+     <LiveStats />
 
       {/* GRADIENT TRANSITION */}
       <div className="w-full h-48 bg-gradient-to-b from-[#000104] to-white" />
